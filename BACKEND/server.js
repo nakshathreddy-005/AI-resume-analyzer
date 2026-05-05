920f349be5fc123e6e0679fc1534f647e80e484c
nakshathrareddy-005 <nakshathrareddy5555@gmail.com>
2026-05-05 10:29:35 +0530

    database connection

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,7 +1,21 @@
 import exp from 'express'
-import { userModel } from './models/userModel'
+import { userModel } from './models/userModel.js'
+import {config} from 'dotenv'
+import {connect} from 'mongoose'
 const app = exp()
 app.use(exp.json())
+config()
 
-const port = 1800
-app.listen(port , () =>console.log(`server listening to port ${port}`))
+const connectDB = async () => {
+  try {
+    await connect(process.env.DB_URL);
+    console.log("DB server connected");
+    //assign port
+    const port = process.env.PORT || 1800;
+    app.listen(port, () => console.log(`server listening on ${port}..`));
+  } catch (err) {
+    console.log("err in db connect", err);
+  }
+};
+
+connectDB();

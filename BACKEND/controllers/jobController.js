7a6bb9c676c6f1bbb62287e909e6ce909c6669ef
unknown <pavithralaghumavarapu@gmail.com>
2026-05-06 10:15:09 +0530

    Added job model job controller and updated analysis controller with job recommendations and modified resumecontroller resume model

---
+++ BACKEND/controllers/jobController.js
@@ -0,0 +1,43 @@
+import Job from "../models/jobModel.js";
+
+export const createJob = async (req, res) => {
+    try {
+        const { title, company, description, requiredSkills, location } = req.body;
+
+        const job = await Job.create({
+            title,
+            company,
+            description,
+            requiredSkills,
+            location,
+        });
+
+        res.status(201).json(job);
+
+    } catch (err) {
+        res.status(500).json({ message: "Job creation failed" });
+    }
+};
+export const getJobs = async (req, res) => {
+    try {
+        const jobs = await Job.find();
+        res.json(jobs);
+    } catch (err) {
+        res.status(500).json({ message: "Failed to fetch jobs" });
+    }
+};
+
+export const getJobById = async (req, res) => {
+    try {
+        const job = await Job.findById(req.params.id);
+
+        if (!job) {
+            return res.status(404).json({ message: "Job not found" });
+        }
+
+        res.json(job);
+
+    } catch (err) {
+        res.status(500).json({ message: "Error fetching job" });
+    }
+};
